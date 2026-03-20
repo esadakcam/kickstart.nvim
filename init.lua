5edd3be6fe3cb3925b04825ddce40fae4166463c
Mehmed Esad Akcam <mehmed.akcam@check24.de>
2026-03-20 15:15:32 +0100

    Add formatters

--- init.lua
+++ init.lua
@@ -752,10 +752,14 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         markdown = { 'prettierd', 'prettier', stop_after_first = true },
         go = { 'goimports', 'gofumpt' },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
         yaml = { 'prettierd', 'prettier', stop_after_first = true },
         ['yaml.ansible'] = { 'ansible-lint' },
         -- Conform can also run multiple formatters sequentially
